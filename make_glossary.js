f97c5146800cff7b3bf8e28a4751feb0ad10e7a1
Hamilton <hamdiggy@gmail.com>
2014-12-03 19:45:53 -0800

    Update make_glossary.js

--- make_glossary.js
+++ make_glossary.js
@@ -1,24 +1,24 @@
 ;(function(w){
 
 /*
-tocel - the id of an element to place the glossary into
-tocterm - the word that will be used for # links and ids
-grabel - the selector for an element to find terms
+tocEl - the id of an element to place the glossary into
+tocTerm - the word that will be used for # links and ids
+grabEl - the selector for an element to find terms
 */
 if(!w.makeGloss) {
-    w.makeGloss = function(tocel,tocterm,grabel) {
-        [].forEach.call(document.querySelectorAll(grabel),function(el,i){
-            var toc = document.getElementById(tocel);
+    w.makeGloss = function(tocEl,tocTerm,grabEl) {
+        [].forEach.call(document.querySelectorAll(grabEl),function(el,i){
+            var toc = document.getElementById(tocEl);
 
             var anchor = document.createElement("a");
             anchor.innerHTML = el.innerHTML;
-            anchor.href = "#"+tocterm+i;
+            anchor.href = "#"+tocTerm+i;
 
             var li = document.createElement("li");
             li.appendChild(anchor);
             toc.appendChild(li);
 
-            el.id = tocterm+i;
+            el.id = tocTerm+i;
         });
     }
 }
